b509c007289a4719c07df8b72914c6490d468363
p0zzle <leon.collazo.are@gmail.com>
2022-02-22 20:15:12 -0300

    Added mock tasks, for the tasks.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,14 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  
   title:string = 'My Task List';
+
   constructor() { }
 
   ngOnInit(): void {
   }
   
   toggleAddTask(){
-    console.log("ToggleAddTask!");
+    console.log("toggleAddTask!");
   }
   
 }
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar modulo de Angular.",
+        day: 'Febrero 26 a las 12.00',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: 'Febrero 26 a las 17.00',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Investigar sobre Bootstrap",
+        day: 'Febrero 26 a las 19.00',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Leer mi libro favorito",
+        day: 'Febrero 26 a las 21.00',
+        reminder: false
+    }
+]
